df64d08c5fff3b1d1b0a513a502b0c598ea06ef7
Raj-nice <rajveersaluja0404@gmail.com>
2025-07-14 14:54:11 +0200

    work

---
+++ Lesson_4/calulate.py
@@ -0,0 +1,9 @@
+user=int(input("Enter the amount you want to withdraw"))
+note1= user//100
+note2= (user%100)//50
+note3= ((user%100)%50)//20
+note4= (((user%100)%50)%20)//10
+print("notes of 100 rupee" , note1)
+print("notes of 50 rupee" , note2)
+print("notes of 20 rupee" , note3)
+print("notes of 10 rupee" , note4)
---
+++ Lesson_4/scores.py
@@ -0,0 +1,9 @@
+print("Enter Marks for Obtained in 4 Subject")
+sub1= int(input("enter Maths marks "))
+sub2= int(input("enter Science marks "))
+sub3= int(input("enter hindi marks "))
+sub4= int(input("enter english marks "))
+sum= sub1+sub2+sub3+sub4
+print("The sum of the subjects: Math, Science, hindi, english are" , sum)
+perc= (sum/400)*100
+print( "Your average percentage is" , perc)
