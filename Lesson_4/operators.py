7d92dc8008321c48480d62abcfcad85f08471609
Raj-nice <rajveersaluja0404@gmail.com>
2025-07-14 14:22:45 +0200

    average

---
+++ Lesson_4/operators.py
@@ -0,0 +1,10 @@
+print ("The height of 5 trees in Jacks yard ")
+t1=98
+t2=94
+t3=41
+t4=96
+t5=11
+total_inches_of_trees= t1+t2+t3+t4+t5 
+print ("The sum of all 5 trees are:", total_inches_of_trees)
+number= 5
+print ("the average of all the trees is:", total_inches_of_trees/number)
