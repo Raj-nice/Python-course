77c8923290e84aa35548768415a83bd1bdfbf621
Raj-nice <rajveersaluja0404@gmail.com>
2025-07-16 14:25:43 +0200

    Lesson_5

---
+++ Lesson_5/Level_2.py
@@ -0,0 +1,14 @@
+user=int(input("Enter the number " ))
+if user%2!=0:
+    print("odd number" ,user)
+else:
+    print("even number", user)
+# user=int(input('enter a n" between 1-50: '))
+
+# if user >15:
+
+#     print('The given number: ',user,'is greater than 15')
+
+# else:
+
+#     print('The given number: ',user,'is lesser than 15')
---
+++ Lesson_5/conditional_statments.py
@@ -0,0 +1,26 @@
+# u=input('do u have a job? ')
+
+# if u=='Yes' or "yes":
+#     print('Hurray! Best of luck to ur journey')
+# else:
+#     print('Lets do Phd')
+
+#Which built-in Python function is used to get input from the user during program execution?
+
+# user= input("Which built-in Python function is used to get input from the user during program execution? ")
+# if user== "input":
+#     print("Good job")
+# else:
+#     print("Don't worry, you will get it next time")
+
+#Profit  loss condition
+
+buy_price=100
+sell_price= 120
+if buy_price<= sell_price:
+    profit=sell_price-buy_price
+    print ("The profit is" ,profit)
+else:
+    print("Their is no profit for the seller")
+    
+
