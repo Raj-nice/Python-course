12933c1da7fca40e0a71103f5a791140b7128908
Raj-nice <rajveersaluja0404@gmail.com>
2025-07-21 13:12:58 +0200

    For Homework

---
+++ Lesson_5/hw_5.py
@@ -0,0 +1,5 @@
+user=int(input("What is the temperature where you are"))
+if user>= 20:
+    print("You dont have to wear a jacket or pullover (unless it is very windy)")
+else:
+    print("You should wear a pullover or jacket asap")
