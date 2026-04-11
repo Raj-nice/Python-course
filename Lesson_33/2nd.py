4ea61db2cbfb21d879a04c3499befe86f336b822
Raj-nice <rajveersaluja0404@gmail.com>
2026-04-11 12:13:31 +0200

    hi

--- Lesson_33/2nd.py
+++ Lesson_33/2nd.py
@@ -4,11 +4,11 @@ pygame.init()
 SCREEN_WIDTH, SCREEN_HEIGHT = 500,500
 
 display_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Addng image and background image")
+pygame.display.set_caption("Adding image and background image")
 
-background_image = pygame.transform.scale(pygame.image.load("background.png").convert(SCREEN_WIDTH, SCREEN_HEIGHT))
+background_image = pygame.transform.scale(pygame.image.load("background.jpg").convert(),(SCREEN_WIDTH, SCREEN_HEIGHT))
 
-penguin_image = pygame.transform.scale(pygame.image.load("penguin.png").convert_alpha(), (200,200))
+penguin_image = pygame.transform.scale(pygame.image.load("penguin.jpeg").convert_alpha(), (200,200))
 
 penguin_rect = penguin_image.get_rect(center = (SCREEN_WIDTH//2, SCREEN_HEIGHT//2 - 30))
 
@@ -29,3 +29,7 @@ def game_loop():
 
         pygame.display.flip()
         clock.tick(30)
+    pygame.quit()
+
+if __name__ == "__main__":
+    game_loop()
